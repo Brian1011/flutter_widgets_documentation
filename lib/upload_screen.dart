f44bdd3426bd11dcd24ca2bee3aea3a60a53fa3d
Brian Mutinda <mutindabrian26@gmail.com>
2022-09-13 20:50:21 +0300

    select multiple images and send to api

--- lib/upload_screen.dart
+++ lib/upload_screen.dart
@@ -11,6 +11,8 @@ class UploadScreen extends StatefulWidget {
 
 class _UploadScreenState extends State<UploadScreen> {
   List<XFile>? documentImages = [];
+  // list of multipart files
+  List<MultipartFile> processedImages = [];
   pickMultipleImages() async {
     try {
       final ImagePicker _picker = ImagePicker();
@@ -18,21 +20,45 @@ class _UploadScreenState extends State<UploadScreen> {
       final List<XFile>? images = await _picker.pickMultiImage();
       if (images != null) {
         documentImages = images;
+        processedImages = [];
         for (var element in images) {
           print('*********IMAGE PATH');
           print(element.path);
+
+          // add to multipart file
+          /* processedImages.add(MultipartFile(
+              File(element.path).readAsBytes().asStream(),
+              File(element.path).lengthSync(),
+              filename: element.path.split("/").last));*/
+
+          processedImages.add(await MultipartFile.fromFile(element.path));
         }
       }
     } catch (e) {
-      print('***********ERROR ${e}');
+      print('***********IMAGES ERROR ${e}');
     }
   }
 
   sendToApi() async {
     Response response;
     var dio = Dio();
-    var data = {'data_field_1': 'one', 'images': documentImages};
-    response = await dio.post('');
+
+    // create data variable
+    var data = {'data_field_1': 'one', 'fileName': processedImages};
+
+    // convert data to form data
+    var newData = FormData.fromMap(data);
+
+    // call the api with the data
+    await dio
+        .post('http://192.168.100.213:8000/api/multiple-image-upload',
+            data: newData)
+        .then((dataResponse) {
+      print(dataResponse);
+    }).catchError((error) {
+      print('******************ERROR');
+      print(error);
+    });
   }
 
   @override
@@ -52,7 +78,17 @@ class _UploadScreenState extends State<UploadScreen> {
                     icon: const Icon(
                       Icons.add_a_photo,
                       size: 40,
-                    )))
+                    ))),
+            const SizedBox(
+              height: 10,
+            ),
+            Center(
+                child: IconButton(
+                    onPressed: sendToApi,
+                    icon: const Icon(
+                      Icons.send,
+                      size: 40,
+                    ))),
           ],
         ),
       ),
