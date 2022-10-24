34dff93bc9ede14ce8d67a917841e0e401af2f85
Brian Mutinda <mutindabrian26@gmail.com>
2022-10-24 10:27:48 +0300

    convert to sentence case function

--- lib/convertToSentenceCase.dart
+++ lib/convertToSentenceCase.dart
@@ -1,5 +1,5 @@
 import 'package:intl/intl.dart' show toBeginningOfSentenceCase;
 
-String convertToSentenceCase(String data){
-  return
+String? convertToSentenceCase(String? data) {
+  return toBeginningOfSentenceCase(data);
 }
