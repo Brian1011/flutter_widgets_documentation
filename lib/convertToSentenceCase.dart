a7d54e127a687e024d82e2675fbf6e03f7311f86
Brian Mutinda <mutindabrian26@gmail.com>
2022-10-24 10:30:52 +0300

    perform operation

--- lib/convertToSentenceCase.dart
+++ lib/convertToSentenceCase.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart' show toBeginningOfSentenceCase;
 
 String? convertToSentenceCase(String? data) {
   return toBeginningOfSentenceCase(data);
+}
+
+main() {
+  debugPrint(convertToSentenceCase("hello world"));
 }
