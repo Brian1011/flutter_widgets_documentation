c517ef5b30b9c555fc026562a8f55d4e34c31289
Brian Mutinda <mutindabrian26@gmail.com>
2022-11-01 08:59:39 +0300

    add watermark image

---
+++ lib/water_mark_image.dart
@@ -0,0 +1,95 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class WaterMarkImage extends StatefulWidget {
+  const WaterMarkImage({Key? key}) : super(key: key);
+
+  @override
+  State<WaterMarkImage> createState() => _WaterMarkImageState();
+}
+
+class _WaterMarkImageState extends State<WaterMarkImage> {
+  List<XFile>? documentImages = [];
+  // list of multipart files
+  List<MultipartFile> processedImages = [];
+  pickMultipleImages() async {
+    try {
+      final ImagePicker _picker = ImagePicker();
+      // Pick multiple images
+      List<XFile>? images = await _picker.pickMultiImage();
+      if (images != null) {
+        documentImages = images;
+        processedImages = [];
+        for (var element in images) {
+          // add to multipart file
+          processedImages.add(MultipartFile(
+              File(element.path).readAsBytes().asStream(),
+              File(element.path).lengthSync(),
+              filename: element.path.split("/").last));
+        }
+      }
+    } catch (e) {
+      print('***********IMAGES ERROR ${e}');
+    }
+  }
+
+  sendToApi() async {
+    Response response;
+    var dio = Dio();
+
+    // create data variable
+    var data = {'data_field_1': 'one', 'fileName[]': processedImages};
+
+    print(data);
+
+    // convert data to form data
+    var newData = FormData.fromMap(data);
+
+    // call the api with the data
+    await dio
+        .post('http://192.168.100.213:8000/api/multiple-image-upload',
+            data: newData)
+        .then((dataResponse) {
+      print(dataResponse);
+    }).catchError((error) {
+      print(error);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("IMAGES"),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            Center(
+                child: IconButton(
+                    onPressed: pickMultipleImages,
+                    icon: const Icon(
+                      Icons.add_a_photo,
+                      size: 40,
+                    ))),
+            const SizedBox(
+              height: 10,
+            ),
+            Center(
+                child: IconButton(
+                    onPressed: sendToApi,
+                    icon: const Icon(
+                      Icons.send,
+                      size: 40,
+                    ))),
+          ],
+        ),
+      ),
+    );
+  }
+}
